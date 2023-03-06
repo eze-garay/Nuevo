4671a5401e94267c32fa7f8b4fc47013d4c2a40c
eze <ezequielgaray37@gmail.com>
2023-03-05 21:01:03 -0300

    nueva entrega

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ let manager = new ProductManager ();
 
 const EjecutApp = async () =>{
     await manager.getProduct();
-    //await manager.addProduct('producto prueba','Este es un producto prueba','200', 'Sin imagen', "abc123",'25',);
+    await manager.addProduct('producto prueba','Este es un producto prueba','200', 'Sin imagen', "abc123",'25',);
 //     await manager.getProduct();
 //     await manager.getProductById(3);
 //     await manager.getProductById(1);
     //await manager.updateProduc(1,'producto prueba2','Este es un producto prueba2','2002', 'Sin imagen2', "abc1232",'252',);
     //await manager.deleteProduct(3);
-    await manager.deleteProduct();
+    //await manager.deleteProduct();
    
 }
 
