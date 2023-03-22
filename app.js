2240d66747b826769ea0d6889caf1c77498c31fb
Ezequiel Garay <ezequielgaray37@gmail.com>
2023-03-22 16:00:46 -0300

    nuevo

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express  from 'express';
+import handlebars from 'express-handlebars';
+//import {server} from 'socket.io';
 import cartsRouters from './src/routes/cart.routes.js'
 import productsRoutes from './src/routes/products.routes.js'
 import __dirname from './utils.js';
@@ -7,10 +9,16 @@ import __dirname from './utils.js';
 
 const app = express ()
 
+//const socketServer = new server
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//app.use(express.static(__dirname + '/src/public/img'))
+app.engine('handlebars',handlebars.engine());
+app.set('views', __dirname + "/views");
+app.set('view engine','handlebars');
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.use('/api/products', productsRoutes)
