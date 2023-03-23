ffd12b1a9e28ce68cda94795175c5624645359b3
Ezequiel Garay <ezequielgaray37@gmail.com>
2023-03-23 20:10:21 -0300

    prueba

---
+++ src/public/js/home.js
@@ -0,0 +1,6 @@
+import manager from "../../service/productManager.js";
+
+let products = manager.products
+
+
+console.log(products)
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,11 +1,25 @@
 
 import express from 'express'
+import manager from '../service/productManager.js'
 
 const router = express.Router()
 
+
+
 router.get('/', (req, res)=>{
-    res.render('index', {})
+    try {
+        let response =  manager.products.map(products=> products.title)
+        if (!response) {
+            return res.status(404).send('error')
+        }
+        return res.status(200).render('home', {
+            manager: response
+        })
+    } catch(error) {
+        return res.status(500).send(error)
+    }
 })
+
 
 // router.get("/message", (req, res)=>{
 //     res.render("messages");
